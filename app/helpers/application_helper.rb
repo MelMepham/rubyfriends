5af94b912018892d8c3349e5a470bd1215de9955
Mel Mepham <mel.mepham@dovetailstudios.com>
2021-12-15 18:00:27 +1300

    conditionally styled header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def active_class(path)
+        if request.path == path
+          return 'bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium'
+        else
+          return 'text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
+        end
+    end
+      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :friends
   root 'home#index'
-  get 'home/about'
+  get 'about', to: 'home#about', as: :about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
